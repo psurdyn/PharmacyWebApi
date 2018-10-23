6802bb9f85dadddd2865fd946c42792d88208aae
Piotr Surdyn <piotrsurdyn@gmail.com>
2018-10-23 12:40:55 +0200

    Added Medicine and Pharmacy Model; added Medicines Controller

---
+++ PharmacyWebApi/Models/Medicine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyWebApi.Models
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string MedicineNumber { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ PharmacyWebApi/Models/Pharmacy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyWebApi.Models
+{
+    public class Pharmacy
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public int DepartmentNumber { get; set; }
+        public IEnumerable<Medicine> AvaiableMedicines { get; set; }
+    }
+}
