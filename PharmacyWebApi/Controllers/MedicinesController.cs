5bf75eef832b20113e83a4f86249c94b4dcbe0c9
Piotr Surdyn <piotrsurdyn@gmail.com>
2018-10-23 13:22:57 +0200

    Added methods to Medicine Controller

--- PharmacyWebApi/Controllers/MedicinesController.cs
+++ PharmacyWebApi/Controllers/MedicinesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PharmacyWebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +10,44 @@ namespace PharmacyWebApi.Controllers
 {
     public class MedicinesController : ApiController
     {
+        private MyDbContext _context;
+
+        public MedicinesController()
+        {
+            _context = new MyDbContext();
+        }
+
+        [HttpGet]
+        [Route("medicines/all")]
+        public IHttpActionResult GetAllMedicines()
+        {
+            var medicines = _context.Medicines.ToList();
+
+            return Ok(medicines);
+        }
+
+        [HttpPost]
+        [Route("medicines/add")]
+        public IHttpActionResult Add(Medicine medicine)
+        {
+            _context.Medicines.Add(medicine);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("medicines/get/{medicineCode}")]
+        public IHttpActionResult GetMedicine(string medicineCode)
+        {
+            var medicine = _context.Medicines.FirstOrDefault(x => x.MedicineNumber == medicineCode);
+
+            if (medicine == null)
+                return NotFound();
+
+            return Ok(medicine);
+        }
 
     }
 }
